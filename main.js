e9e5abba96e649629e9c598c307f14fe03bce065
GilmantLucas <lucas.gilmant@gmail.com>
2019-02-13 11:40:04 +0100

    Update main.js

--- main.js
+++ main.js
@@ -9,12 +9,6 @@ const db = low(adapter);
 
 db.defaults({ persos: []})
     .write();
-	
-//HEROKU
-app.set('port',(process.env.PORT || 5000));    
-app.listen(app.get('port'), function(){
-    console.log("Bot en fonctionnement sur le port"+ app.get('port'));
-});
 
 var bot = new Discord.Client();
 var prefix = ("!");
